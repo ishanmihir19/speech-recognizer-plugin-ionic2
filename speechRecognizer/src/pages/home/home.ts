39a0ed672f8774dcd48161fab00b3482cb047480
ishanmihir <mihir.ishan@yahoo.in>
2016-11-24 03:19:04 +0530

    Added plugin and the working code in home component

--- speechRecognizer/src/pages/home/home.ts
+++ speechRecognizer/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
 
-import { NavController } from 'ionic-angular';
+declare var SpeechRecognition: any;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  recognizedText: string;
+  recognition: any;
+
+  constructor(public navCtrl: NavController, public platform: Platform) {
+
+  }
+
+  startSpeechRecognition() {
+    this.platform.ready().then(() => {
+      this.recognition = new SpeechRecognition();
+      this.recognition.lang = 'en-US';
+      this.recognition.onnomatch = (event => {
+        console.log('No match found.');
+      });
+      this.recognition.onerror = (event => {
+        console.log('Error happens.');
+      });
+      this.recognition.onresult = (event => {
+        if (event.results.length > 0) {
+          console.log('Output STT: ', event.results[0][0].transcript);
+        }
+      });
+      this.recognition.start();
+    });
   }
 
 }
